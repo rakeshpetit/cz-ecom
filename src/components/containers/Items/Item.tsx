0bcba2348892b89108a476f5ad7a4172fe362a7e
Rakesh <rakeshpetit@gmail.com>
2019-07-31 11:02:15 +0100

    navigation to details

--- src/components/containers/Items/Item.tsx
+++ src/components/containers/Items/Item.tsx
@@ -8,20 +8,5 @@ const Item = () => {
     );
 };
 
-Item.navigationOptions: ({ navigation }) => {
-    return {
-        title: "Item Details",
-        headerTitleStyle: {
-    textAlign: "left",
-    fontFamily: "OpenSans-Regular",
-    fontSize: 24
-  },
-        headerLeft: (
-            <TouchableOpacity onPress={() => navigation.goBack()}>
-                <Text>Back</Text>
-            </TouchableOpacity>
-        )
-    };
-};
 
 export default Item;
--- src/components/routes/Routes.tsx
+++ src/components/routes/Routes.tsx
@@ -4,13 +4,44 @@ import { TouchableOpacity, Text } from "react-native";
 import Home from "../containers/Home/Home";
 import Item from "../containers/Items/Item";
 
+const ItemStackNavigator = createStackNavigator(
+    {
+        ItemScreen: {
+            screen: Item,
+            navigationOptions: ({ navigation }) => {
+                return {
+                    title: "Item Details",
+                    headerTitleStyle: {
+                        textAlign: "left",
+                        fontSize: 24
+                    },
+                    headerLeft: (
+                        <TouchableOpacity
+                            onPress={() =>
+                                navigation.navigate(
+                                    "Home"
+                                )
+                            }>
+                            <Text>Back</Text>
+                        </TouchableOpacity>
+                    )
+                };
+            }
+        }
+    },
+    {
+        headerMode: "float"
+    }
+);
+
+
 const Routes = createStackNavigator(
     {
         Home: {
             screen: Home
         },
         Item: {
-            screen: Item
+            screen: ItemStackNavigator
         }
     },
     {
